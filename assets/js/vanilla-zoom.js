9751edf937101c5cd8a753d46aa60d87f430a3c6
Dejatori <deivitsc@gmail.com>
2023-10-05 16:27:00 -0500

    Mejoras de front-end

--- assets/js/vanilla-zoom.js
+++ assets/js/vanilla-zoom.js
@@ -1,80 +1,81 @@
-(function (window) {
+(function(window){
     function define_library() {
-        let vanillaZoom = {};
-        vanillaZoom.init = function (el) {
+        var vanillaZoom = {};
+        vanillaZoom.init = function(el) {
 
-            let container = document.querySelector(el);
-            if (!container) {
+            var container = document.querySelector(el);
+            if(!container) {
                 console.error('No container element. Please make sure you are using the right markup.');
                 return;
             }
 
-            let firstSmallImage = container.querySelector('.small-preview');
-            let zoomedImage = container.querySelector('.zoomed-image');
+            var firstSmallImage = container.querySelector('.small-preview');
+            var zoomedImage = container.querySelector('.zoomed-image');
 
-            if (!zoomedImage) {
+            if(!zoomedImage) {
                 console.error('No zoomed image element. Please make sure you are using the right markup.');
                 return;
             }
 
-            if (!firstSmallImage) {
+            if(!firstSmallImage) {
                 console.error('No preview images on page. Please make sure you are using the right markup.');
                 return;
-            } else {
-                // Set the source of the zoomed image.
-                zoomedImage.style.backgroundImage = 'url(' + firstSmallImage.src + ')';
-                firstSmallImage.classList.add('active');
             }
+            else {
+                // Set the source of the zoomed image.
+                zoomedImage.style.backgroundImage = 'url('+ firstSmallImage.src +')';
+                firstSmallImage.classList.add('active');
+            }   
 
             // Change the selected image to be zoomed when clicking on the previews.
             container.addEventListener("click", function (event) {
-                let elem = event.target;
+                var elem = event.target;
 
                 if (elem.classList.contains("small-preview")) {
-
-                    let allSmallPreviews = container.querySelectorAll(".small-preview");
-
+                    
+                    var allSmallPreviews = container.querySelectorAll(".small-preview");
+                    
                     allSmallPreviews.forEach(function (preview) {
                         preview.classList.remove('active');
                     })
-
+                    
                     elem.classList.add('active');
-
-                    let imageSrc = elem.src;
-                    zoomedImage.style.backgroundImage = 'url(' + imageSrc + ')';
+                    
+                    var imageSrc = elem.src;
+                    zoomedImage.style.backgroundImage = 'url('+ imageSrc +')';
                 }
             });
-
+            
             // Zoom image on mouse enter.
-            zoomedImage.addEventListener('mouseenter', function (e) {
-                this.style.backgroundSize = "250%";
+            zoomedImage.addEventListener('mouseenter', function(e) {
+                this.style.backgroundSize = "250%"; 
             }, false);
 
 
             // Show different parts of image depending on cursor position.
-            zoomedImage.addEventListener('mousemove', function (e) {
-
+            zoomedImage.addEventListener('mousemove', function(e) {
+                
                 // getBoundingClientReact gives us various information about the position of the element.
-                let dimentions = this.getBoundingClientRect();
+                var dimentions = this.getBoundingClientRect();
 
                 // Calculate the position of the cursor inside the element (in pixels).
-                let x = e.clientX - dimentions.left;
-                let y = e.clientY - dimentions.top;
+                var x = e.clientX - dimentions.left;
+                var y = e.clientY - dimentions.top;
 
                 // Calculate the position of the cursor as a percentage of the total width/height of the element.
-                let xpercent = Math.round(100 / (dimentions.width / x));
-                let ypercent = Math.round(100 / (dimentions.height / y));
+                var xpercent = Math.round(100 / (dimentions.width / x));
+                var ypercent = Math.round(100 / (dimentions.height / y));
 
                 // Update the background position of the image.
-                this.style.backgroundPosition = xpercent + '% ' + ypercent + '%';
-
+                this.style.backgroundPosition = xpercent+'% ' + ypercent+'%';
+            
             }, false);
 
 
             // When leaving the container zoom out the image back to normal size.
-            zoomedImage.addEventListener('mouseleave', function (e) {
-                this.style.backgroundSize = "contain";
-                this.style.backgroundPosition = "left center";
+            zoomedImage.addEventListener('mouseleave', function(e) {
+                this.style.backgroundSize = "contain"; 
+                this.style.backgroundPosition = "left center"; 
             }, false);
 
         }
@@ -82,9 +83,10 @@
     }
 
     // Add the vanillaZoom object to global scope.
-    if (typeof (vanillaZoom) === 'undefined') {
+    if(typeof(vanillaZoom) === 'undefined') {
         window.vanillaZoom = define_library();
-    } else {
+    }
+    else{
         console.log("Library already defined.");
     }
 })(window);
